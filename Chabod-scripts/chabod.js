daa72c480210586ebdc240ad9c0a28e0a5547353
Okpainmo <42122017+Okpainmo@users.noreply.github.com>
2021-02-13 17:50:12 -0800

    Added a new chabod.js file

---
+++ Chabod-scripts/chabod.js
@@ -0,0 +1,18 @@
+window.onscroll = function() {scrollBuddy()};
+
+function scrollBuddy() {
+  if (document.body.scrollTop > 632 || document.documentElement.scrollTop > 632) {
+    document.getElementById("mynav").style.backgroundColor = "rgba(255, 255, 255, 0.96)";
+  } else {
+    document.getElementById("mynav").style.backgroundColor = "transparent";
+  }
+}
+
+const bars = document.getElementById("bars");
+
+function navClick() {
+    document.getElementById("navbarSupportedContent").style.backgroundColor = "rgba(255, 255, 255, 0.87)";
+    document.getElementById("navbarSupportedContent").style.padding = "20px 0" 
+}
+
+bars.addEventListener("click", navClick)
