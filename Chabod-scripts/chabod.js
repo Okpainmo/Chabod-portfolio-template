5409e9e28d223f8d0e45edf68d6b74a306f9c607
Okpainmo <okpainmoandrew@gmail.com>
2021-02-26 03:21:36 +0100

    i made some more brushups on chabod. thing i'am done with ui/ux

--- Chabod-scripts/chabod.js
+++ Chabod-scripts/chabod.js
@@ -1,4 +1,9 @@
-window.onscroll = function() {scrollBuddy()};
+// VARIABLES BEGIN//  // VARIABLES BEGIN//
+
+const bars = document.getElementById("bars");
+
+//VARIABLES END//  //VARIABLES END//
+
 
 function scrollBuddy() {
   if (document.body.scrollTop > 300 || document.documentElement.scrollTop > 300) {
@@ -8,11 +13,22 @@ function scrollBuddy() {
   }
 }
 
-const bars = document.getElementById("bars");
+function floatTopButton() {
+  if (document.body.scrollTop > 1000 || document.documentElement.scrollTop > 1000) {
+    document.getElementById("floatButton").style.display = "block";
+  }
+  else {
+    document.getElementById("floatButton").style.display = "none";
+  }
+}
 
 function navClick() {
     document.getElementById("navbarSupportedContent").style.backgroundColor = "rgba(255, 255, 255, 0.87)";
-    document.getElementById("navbarSupportedContent").style.padding = "20px 0" 
+    document.getElementById("navbarSupportedContent").style.padding = "20px 0"; 
 }
 
-bars.addEventListener("click", navClick)
+//EVENT LISTENERS BEGIN//  //EVENT LISTENERS BEGIN //
+
+window.addEventListener ("scroll", floatTopButton);
+window.addEventListener ("scroll", scrollBuddy);
+bars.addEventListener("click", navClick);
