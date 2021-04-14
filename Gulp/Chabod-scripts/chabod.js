2cf77c6352ed5111ef8ea1b763599836c0afc2e5
Okpainmo <okpainmoandrew@gmail.com>
2021-04-14 22:22:00 +0100

    more progress

--- Gulp/Chabod-scripts/chabod.js
+++ Gulp/Chabod-scripts/chabod.js
@@ -2,16 +2,45 @@
 $(document).ready( function() {
   $(window).scroll(function(){
     if (document.body.scrollTop > 300 || document.documentElement.scrollTop > 300) {
-      $("#mynav").css("backgroundColor","rgba(255, 255, 255, 0.96");
-      $("#mynav").css("boxShadow", "0 0 15px 1px hsl(0, 2%, 89%)");
+      $("#myNav").css("backgroundColor","rgba(255, 255, 255, 0.96");
+      $("#myNav").css("boxShadow", "0 0 15px 1px hsl(0, 2%, 89%)");
     }
     else {
-      $("#mynav").css("backgroundColor", "transparent");
-      $("#mynav").css("boxShadow", "none");
+      $("#myNav").css("backgroundColor", "transparent");
+      $("#myNav").css("boxShadow", "none");
     }
-}
+  }
 )
 })
+
+// $(document).ready( function() {
+//   $(window).scroll(function(){
+//     if (document.body.scrollTop > 150 || document.documentElement.scrollTop > 150) {
+//       $(".works-heading").css("position","fixed");
+//       $(".works-heading").css("top", 0);
+//       $(".works-heading").css("left", 0);
+//     }
+//     else {
+//       $("#myNav").css("backgroundColor", "transparent");
+//       $("#myNav").css("boxShadow", "none");
+//     }
+//   }
+// )
+// })
+// $(document).ready( function() {
+//   $(window).scroll(function(){
+//     if (document.body.scrollTop > 150 || document.documentElement.scrollTop > 150) {
+//       $(".works-heading").css("position","fixed");
+//       $(".works-heading").css("top", 0);
+//       $(".works-heading").css("left", 0);
+//     }
+//     else {
+//       $("#myNav").css("backgroundColor", "transparent");
+//       $("#myNav").css("boxShadow", "none");
+//     }
+//   }
+// )
+// })
 
 //float button function.
 $(document).ready(
@@ -37,11 +66,50 @@ $(document).ready(
   )
 )
 
-$(".card-pic-case-child").hover(
-  function() {
-    $(this).animate({opacity:"1"}, "slow")
-  },
-  function() {
-    $(this).animate({opacity:"0"}, "slow")
-  }
+//works section hover effect
+$(document).ready(
+  $(".card-pic-case-child").hover(
+    function() {
+      $(this).animate({opacity:"1"}, "slow")
+    },
+    function() {
+      $(this).animate({opacity:"0"}, "slow")
+    }
+  )
+)
+
+
+$(document).ready(
+  $("#closeIcon").click (
+    function() {
+      $("#popUpOverlay").fadeOut()
+    }
+  )
+)
+
+$(document).ready(
+  $("#work1").click(
+      function() {
+      $("#workInfo1").css("display", "block"),
+      $("#workInfo2").css("display", "none"),
+      $("#workInfo3").css("display", "none"),
+      $("#popUpOverlay").fadeIn() 
+    }
+  ),
+  $("#work2").click(
+    function() {
+    $("#workInfo1").css("display", "none"),
+    $("#workInfo2").css("display", "block"),
+    $("#workInfo3").css("display", "none"),
+    $("#popUpOverlay").fadeIn() 
+    }
+  ),
+  $("#work3").click(
+    function() {
+    $("#workInfo1").css("display", "none"),
+    $("#workInfo2").css("display", "none"),
+    $("#workInfo3").css("display", "block"),
+    $("#popUpOverlay").fadeIn() 
+    }
+  )
 )
