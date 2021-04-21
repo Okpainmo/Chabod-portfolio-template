ad39fe3ece02b90e4b659785bc7e6946a10e27c8
Okpainmo <okpainmoandrew@gmail.com>
2021-04-21 17:08:04 +0100

    more progress

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,13 @@
+'use strict';
+
 // variables
 const gulp = require('gulp');
 const sass = require('gulp-sass');
 const autoprefixer = require('gulp-autoprefixer');
-const sassPartialsImported = require('gulp-sass-partials-imported');
-
+// const sassPartialsImported = require('gulp-sass-partials-imported');
 // const browserSync = require('browser-sync').create();
 
+sass.compiler = require('sass');
 
 // custom-hello task
 gulp.task('hello', function (done) {
@@ -13,6 +15,15 @@ gulp.task('hello', function (done) {
     done();
     });
 
+//sass task with autoprefixer
+gulp.task('sass', function () {
+    return gulp.src('Gulp/Chabod-styles/Chabod-scss/**/*.scss')
+      .pipe(sass().on('error', sass.logError))
+      .pipe(autoprefixer({
+        cascade: false
+    }))
+      .pipe(gulp.dest('Gulp/Chabod-styles/Chabod-css'));
+  });
 
 // gulp.task('task-name', function () {   
 //     return gulp.src('source-files')  //Get source files
@@ -20,30 +31,8 @@ gulp.task('hello', function (done) {
 //             .pipe(gulp.dest('destination'))  //Output to destination
 //     })
 
-// sass task
-gulp.task('sass', function () {   
-    return gulp.src('Gulp/Chabod-styles/Chabod-scss/**/*.scss')
-            .pipe(sassPartialsImported('Gulp/Chabod-style/Chabod.scss'))
-            .pipe(sass())
-            .pipe(gulp.dest('Gulp/Chabod-styles/Chabod-css')) 
-    })
 
 // watch task
 gulp.task('watch', function() {
     gulp.watch('Gulp/Chabod-styles/Chabod-scss/**/*.scss', gulp.series('sass'));
 })
-
-// autoprefixer task
-gulp.task('sass', function () {   
-    return gulp.src('Gulp/Chabod-styles/Chabod-scss/**/*.scss')
-           .pipe(sass())
-           .pipe(autoprefixer())
-           .pipe(gulp.dest('Gulp/Chabod-styles/Chabod-css'))
- });
-
-//  gulp.task('serve', function(){
-//     browserSync.init({
-//         server: 'src',
-//         port: 4000
-//     });
-// })
